7507100f9a9aba2668b39d6ddf0e3793127b62f5
serdar <serdarsarsar@gmx.de>
2018-09-04 15:17:13 +0200

    weitere activitys hinzugefügt

---
+++ app/src/main/java/ks/battleshipmobile/Anleitungsbildschirm.java
@@ -0,0 +1,13 @@
+package ks.battleshipmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Anleitungsbildschirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anleitungsbildschirm);
+    }
+}
