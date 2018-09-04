72d7fa77220cd336bb54b42272e6888a6f7e4a3b
serdar <serdarsarsar@gmx.de>
2018-09-04 14:59:42 +0200

    skelett weiter ausgebaut

--- app/src/main/java/ks/battleshipmobile/Startbildschirm.java
+++ app/src/main/java/ks/battleshipmobile/Startbildschirm.java
@@ -2,12 +2,32 @@ package ks.battleshipmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Startbildschirm extends AppCompatActivity {
+public class Startbildschirm extends AppCompatActivity implements View.OnClickListener {
+
+    Button start;
+    Button anleitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startbildschirm);
+
+        start = findViewById(R.id.spielstartenbutton);
+        anleitung = findViewById(R.id.anleitungbutton);
+
+        start.setOnClickListener(this);
+        anleitung.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View e) {
+        if (e.equals(start)) {
+
+        }
+        if (e.equals(anleitung)) {
+
+        }
     }
 }
