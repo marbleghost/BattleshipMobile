977714b78e51862e5bde42c4663c05cf9e902bfb
serdar <serdarsarsar@gmx.de>
2018-09-07 12:03:44 +0200

    TextView für die Spielemodi hinzugefügt

--- app/src/main/java/ks/battleshipmobile/Startbildschirm.java
+++ app/src/main/java/ks/battleshipmobile/Startbildschirm.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Startbildschirm extends AppCompatActivity implements View.OnClickListener {
 
     FloatingActionButton start;
     FloatingActionButton anleitung;
     FloatingActionButton leichteki, mittlereki, schwierigeki;
+    TextView textleichteki, textmittlereki, textschwierigeki;
     boolean schwierigkeitsauswahloffen = false;
 
     Animation fab_oeffnen;
@@ -31,9 +33,14 @@ public class Startbildschirm extends AppCompatActivity implements View.OnClickLi
          */
         start = (FloatingActionButton) findViewById(R.id.button_spielauswahl);
         anleitung = (FloatingActionButton) findViewById(R.id.button_anleitung);
+
         leichteki = (FloatingActionButton) findViewById(R.id.button_kileicht);
         mittlereki = (FloatingActionButton) findViewById(R.id.button_kimittel);
         schwierigeki = (FloatingActionButton) findViewById(R.id.button_kischwer);
+
+        textleichteki = (TextView) findViewById(R.id.textkileicht);
+        textmittlereki = (TextView) findViewById(R.id.textkimittel);
+        textschwierigeki = (TextView) findViewById(R.id.textkischwer);
 
         start.setOnClickListener(this);
         anleitung.setOnClickListener(this);
@@ -52,10 +59,13 @@ public class Startbildschirm extends AppCompatActivity implements View.OnClickLi
             //Snackbar.make(e, "was auch immer das ist", Snackbar.LENGTH_LONG).setAction("action", null).show();
             if (schwierigkeitsauswahloffen == false) { // Oeffnet das Auswahlfenster für den Schwierigkeitsgrad.
                 leichteki.startAnimation(fab_oeffnen);
+                textleichteki.startAnimation(fab_oeffnen);
                 leichteki.setClickable(true);
                 mittlereki.startAnimation(fab_oeffnen);
+                textmittlereki.startAnimation(fab_oeffnen);
                 mittlereki.setClickable(true);
                 schwierigeki.startAnimation(fab_oeffnen);
+                textschwierigeki.startAnimation(fab_oeffnen);
                 schwierigeki.setClickable(true);
                 anleitung.setVisibility(View.GONE);
                 anleitung.setClickable(false);
@@ -63,10 +73,13 @@ public class Startbildschirm extends AppCompatActivity implements View.OnClickLi
             }
             else { //Schließt das Auswahlfenster, wenn es bereits offen ist.
                 leichteki.startAnimation(fab_schliessen);
+                textleichteki.startAnimation(fab_schliessen);
                 leichteki.setClickable(false);
                 mittlereki.startAnimation(fab_schliessen);
+                textmittlereki.startAnimation(fab_schliessen);
                 mittlereki.setClickable(false);
                 schwierigeki.startAnimation(fab_schliessen);
+                textschwierigeki.startAnimation(fab_schliessen);
                 schwierigeki.setClickable(false);
                 anleitung.setVisibility(View.VISIBLE);
                 anleitung.setClickable(true);
