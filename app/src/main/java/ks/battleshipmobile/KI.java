d1fbfd0d4df2c2c1cefa3bfb8ea34971a89a6743
serdar <serdarsarsar@gmx.de>
2018-09-20 15:51:03 +0200

    kiMittel weiterbearbeitet

--- app/src/main/java/ks/battleshipmobile/KI.java
+++ app/src/main/java/ks/battleshipmobile/KI.java
@@ -1,5 +1,6 @@
 package ks.battleshipmobile;
 
+import android.os.health.SystemHealthManager;
 import android.widget.Button;
 
 import java.util.Random;
@@ -9,8 +10,8 @@ public class KI {
     Spiellogik logik = new Spiellogik();
     Random random = new Random();
 
-    int tempn = 0;
-    int tempm = 0;
+    int tempn = -1;
+    int tempm = -1;
 
 
     public int randomInt() {
@@ -46,17 +47,22 @@ public class KI {
             int n = randomInt();
             int m = randomInt();
 
-            if (tempn == 0 && tempm == 0) {
+            if (tempn == -1 && tempm == -1) {
                 if (tempInt[n][m] != 2 && tempInt[n][m] != 3 && tempInt[n][m] != 6) {
                     logik.schussAbgefeuert(n, m, tempInt, tempButton);
+                    System.out.println(n + " " + m);
+                    System.out.println(tempInt[n][m]);
                     if (logik.treffer == false) {
                         versuche--;
                     }
                     else {
-                        tempn = n;
-                        tempm = m;
-
                         logik.testeSchiffVersenkt(n, m, tempInt, tempButton);
+
+                        if (logik.testeSchiffVersenkt(n, m, tempInt, tempButton) == false) {
+                            tempn = n;
+                            tempm = m;
+                        }
+
                         if (logik.gewonnen(tempInt)) {
                             System.out.println("KI hat gewonnen");
                         }
@@ -67,17 +73,25 @@ public class KI {
                 if (tempn>0) {
                     if (tempInt[tempn-1][tempm] != 2 && tempInt[tempn-1][tempm] != 3 && tempInt[tempn-1][tempm] != 6) {
                         logik.schussAbgefeuert(tempn-1, tempm, tempInt, tempButton);
+                        System.out.println(tempn-1 + " " + tempm);
+                        System.out.println(tempInt[tempn-1][tempm]);
                         if (logik.treffer == false) {
                             versuche--;
+                            break;
                         }
                         else {
-                            tempn = tempn-1;
-
                             logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton);
+
                             if (logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton)) {
-                                tempn = 0;
-                                tempm = 0;
+                                tempn = -1;
+                                tempm = -1;
+                                n = randomInt();
+                                m = randomInt();
                             }
+                            else {
+                                tempn = tempn-1;
+                            }
+
                             if (logik.gewonnen(tempInt)) {
                                 System.out.println("KI hat gewonnen");
                             }
@@ -87,17 +101,25 @@ public class KI {
                 if (tempn<logik.feldgroesse-1) {
                     if (tempInt[tempn+1][tempm] != 2 && tempInt[tempn+1][tempm] != 3 && tempInt[tempn+1][tempm] != 6) {
                         logik.schussAbgefeuert(tempn+1, tempm, tempInt, tempButton);
+                        System.out.println(tempn+1 + " " + tempm);
+                        System.out.println(tempInt[tempn+1][tempm]);
                         if (logik.treffer == false) {
                             versuche--;
+                            break;
                         }
                         else {
-                            tempn = tempn+1;
 
                             logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton);
                             if (logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton)) {
-                                tempn = 0;
-                                tempm = 0;
+                                tempn = -1;
+                                tempm = -1;
+                                n = randomInt();
+                                m = randomInt();
                             }
+                            else {
+                                tempn = tempn+1;
+                            }
+
                             if (logik.gewonnen(tempInt)) {
                                 System.out.println("KI hat gewonnen");
                             }
@@ -108,17 +130,25 @@ public class KI {
                 if (tempm>0) {
                     if (tempInt[tempn][tempm-1] != 2 && tempInt[tempn][tempm-1] != 3 && tempInt[tempn][tempm-1] != 6) {
                         logik.schussAbgefeuert(tempn, tempm-1, tempInt, tempButton);
+                        System.out.println(tempn + " " + (tempm-1));
+                        System.out.println(tempInt[tempn][tempm-1]);
                         if (logik.treffer == false) {
                             versuche--;
+                            break;
                         }
                         else {
-                            tempm = tempm-1;
 
                             logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton);
                             if (logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton)) {
-                                tempn = 0;
-                                tempm = 0;
+                                tempn = -1;
+                                tempm = -1;
+                                n = randomInt();
+                                m = randomInt();
                             }
+                            else {
+                                tempm = tempm-1;
+                            }
+
                             if (logik.gewonnen(tempInt)) {
                                 System.out.println("KI hat gewonnen");
                             }
@@ -128,17 +158,26 @@ public class KI {
                 if (tempm<logik.feldgroesse-1) {
                     if (tempInt[tempn][tempm+1] != 2 && tempInt[tempn][tempm+1] != 3 && tempInt[tempn][tempm+1] != 6) {
                         logik.schussAbgefeuert(tempn, tempm+1, tempInt, tempButton);
+                        System.out.println(tempn + " " + (tempm+1));
+                        System.out.println(tempInt[tempn][tempm+1]);
                         if (logik.treffer == false) {
                             versuche--;
+                            break;
                         }
                         else {
-                            tempm = tempm+1;
+
 
                             logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton);
                             if (logik.testeSchiffVersenkt(tempn, tempm, tempInt, tempButton)) {
-                                tempn = 0;
-                                tempm = 0;
+                                tempn = -1;
+                                tempm = -1;
+                                n = randomInt();
+                                m = randomInt();
                             }
+                            else {
+                                tempm = tempm+1;
+                            }
+
                             if (logik.gewonnen(tempInt)) {
                                 System.out.println("KI hat gewonnen");
                             }
