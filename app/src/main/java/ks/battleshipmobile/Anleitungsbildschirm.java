0578c9b3f6630a70ff9dbefe1ce4483892ff9c04
Kirsten <kapeine@gmx.de>
2018-09-23 18:43:19 +0200

    Anleitungsbildschirm fertig

--- app/src/main/java/ks/battleshipmobile/Anleitungsbildschirm.java
+++ app/src/main/java/ks/battleshipmobile/Anleitungsbildschirm.java
@@ -6,16 +6,33 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Anleitungsbildschirm extends AppCompatActivity {
 
     Button hauptmenue;
+    TextView anleitungstart;
+    TextView schiffesetzen, schiffesetzen2, schiffesetzen3;
+    TextView dasspiel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); // sorgt dafür, dass die Titelleiste mit dem Appnamen nicht mehr oben angezeigt wird.
         setContentView(R.layout.activity_anleitungsbildschirm);
+
+        //ANleitungstexte
+        anleitungstart = findViewById(R.id.text_start);
+        anleitungstart.setText(R.string.anleitungtext);
+
+        schiffesetzen = findViewById(R.id.text_schiffesetzen);
+        schiffesetzen.setText(R.string.anleitung_schiffesetzentext);
+        schiffesetzen2 = findViewById(R.id.text_schiffesetzen2);
+        schiffesetzen2.setText(R.string.anleitung_schiffesetzentext2);
+        schiffesetzen3 = findViewById(R.id.text_schiffesetzen3);
+        schiffesetzen3.setText(R.string.anleitung_schiffesetzentext3);
+        dasspiel = findViewById(R.id.text_dasspiel);
+        dasspiel.setText(R.string.dasspieltext);
 
         //Button um zurueck ins Hauptmenue zu gelangen
         hauptmenue = findViewById(R.id.zurueck);
